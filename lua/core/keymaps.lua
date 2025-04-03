7028555c59528726b0e9b4b1c89e858f03d89afd
ThatAmuzak <rishavbanerjee1710@gmail.com>
2025-04-03 16:46:37 -0700

    more keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -24,3 +24,16 @@ vim.keymap.set('v', '<', '<gv', opts)
 vim.keymap.set('v', '>', '>gv', opts)
 
 vim.keymap.set('v', 'p', '"_dP', opts)
+vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
+
+vim.keymap.set("n", "d;", "d$", opts)
+
+vim.keymap.set('n', '<leader>v', '<C-w>v', opts) 
+vim.keymap.set('n', '<leader>h', '<C-w>s', opts)
+vim.keymap.set('n', '<leader>se', '<C-w>=', opts) 
+vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) 
+
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)

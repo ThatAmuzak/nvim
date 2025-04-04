b3d4f34d0c2afb24bc80b800d3d5aa03593cfe3c
ThatAmuzak <rishavbanerjee1710@gmail.com>
2025-04-03 20:01:16 -0700

    whole buncha stuff

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,39 +1,49 @@
 vim.g.mapleader = ' '
 vim.g.maploclleader = ' '
-vim.keymap.set({'n', 'v'}, '<Space>', '<Nop>', {silent=true})
+vim.keymap.set({'n', 'v'}, '<Space>', '<Nop>', { silent = true })
 
-local opts = {noremap=true, silent=true}
+local base_opts = { noremap = true, silent = true }
 
-vim.keymap.set('n', '<leader>fs', '<cmd> w <CR>', opts)
-vim.keymap.set('n', '<leader>fq', '<cmd> x <CR>', opts)
-vim.keymap.set('n', '<leader>qq', '<cmd> wqa <CR>', opts)
-vim.keymap.set('n', 'x', '"_x', opts)
+local function map(mode, lhs, rhs, desc)
+    vim.keymap.set(mode, lhs, rhs, vim.tbl_extend('force', base_opts, { desc = desc }))
+end
 
-vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
-vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
+-- Save and quit
+map('n', '<leader>ww', '<cmd> wa <CR>', "Save file")
+map('n', '<leader>qq', '<cmd> wqa <CR>', "Save all and quit")
 
-vim.keymap.set('n', 'n', 'nzzzv', opts)
-vim.keymap.set('n', 'N', 'Nzzzv', opts)
+-- Editing enhancements
+map('n', 'x', '"_x', "Delete character without yanking")
+map('n', '<C-d>', '<C-d>zz', "Scroll down and center")
+map('n', '<C-u>', '<C-u>zz', "Scroll up and center")
+map('n', 'n', 'nzzzv', "Next search result centered")
+map('n', 'N', 'Nzzzv', "Previous search result centered")
+map('n', 'd;', 'd$', "Delete until end of line")
 
-vim.keymap.set('n', '<Up>', ':resize -2<CR>', opts)
-vim.keymap.set('n', '<Down>', ':resize +2<CR>', opts)
-vim.keymap.set('n', '<Left>', ':vertical resize -2<CR>', opts)
-vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
+-- Window resizing
+map('n', '<Up>', ':resize -2<CR>', "Resize window up")
+map('n', '<Down>', ':resize +2<CR>', "Resize window down")
+map('n', '<Left>', ':vertical resize -2<CR>', "Resize window left")
+map('n', '<Right>', ':vertical resize +2<CR>', "Resize window right")
 
-vim.keymap.set('v', '<', '<gv', opts)
-vim.keymap.set('v', '>', '>gv', opts)
+-- Indentation in visual mode
+map('v', '<', '<gv', "Indent left and stay selected")
+map('v', '>', '>gv', "Indent right and stay selected")
+map('v', 'p', '"_dP', "Paste without overwriting register")
 
-vim.keymap.set('v', 'p', '"_dP', opts)
-vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
+-- LSP hover
+map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', "LSP Hover")
 
-vim.keymap.set("n", "d;", "d$", opts)
+-- Window management
+map('n', '<leader>v', '<C-w>v', "Split window vertically")
+map('n', '<leader>h', '<C-w>s', "Split window horizontally")
+map('n', '<leader>se', '<C-w>=', "Make splits equal size")
+map('n', '<leader>xs', ':close<CR>', "Close current split")
+map('n', '<C-k>', ':wincmd k<CR>', "Move to window above")
+map('n', '<C-j>', ':wincmd j<CR>', "Move to window below")
+map('n', '<C-h>', ':wincmd h<CR>', "Move to window left")
+map('n', '<C-l>', ':wincmd l<CR>', "Move to window right")
 
-vim.keymap.set('n', '<leader>v', '<C-w>v', opts) 
-vim.keymap.set('n', '<leader>h', '<C-w>s', opts)
-vim.keymap.set('n', '<leader>se', '<C-w>=', opts) 
-vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) 
+-- Clear search highlight
+map('n', '<Esc>', '<cmd>nohlsearch<CR>', "Clear search highlight")
 
-vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
-vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
-vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
-vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)

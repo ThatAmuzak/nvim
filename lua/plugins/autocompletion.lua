0ee9e9b66a90f5afceda95864d303776ed092c27
ThatAmuzak <rishavbanerjee1710@gmail.com>
2025-06-04 00:59:55 -0700

    snippets for unity
    fixed some keymaps and moved to my fork, cuz I'm too lazy to setup a new
    file

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -18,7 +18,7 @@ return { -- Autocompletion
 				--    See the README about individual language/framework/plugin snippets:
 				--    https://github.com/rafamadriz/friendly-snippets
 				{
-					"rafamadriz/friendly-snippets",
+					"ThatAmuzak/friendly-snippets",
 					config = function()
 						require("luasnip.loaders.from_vscode").lazy_load()
 					end,
@@ -40,6 +40,7 @@ return { -- Autocompletion
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
 		luasnip.config.setup({})
+		luasnip.filetype_extend("cs", { "unity" })
 
 		local kind_icons = {
 			Text = "󰉿",
@@ -82,9 +83,9 @@ return { -- Autocompletion
 			-- No, but seriously. Please read `:help ins-completion`, it is really good!
 			mapping = cmp.mapping.preset.insert({
 				-- Select the [n]ext item
-				-- ["<C-n>"] = cmp.mapping.select_next_item(),
+				["<C-n>"] = cmp.mapping.select_next_item(),
 				-- Select the [p]revious item
-				-- ["<C-p>"] = cmp.mapping.select_prev_item(),
+				["<C-p>"] = cmp.mapping.select_prev_item(),
 
 				-- Scroll the documentation window [b]ack / [f]orward
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -129,18 +130,15 @@ return { -- Autocompletion
 				--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
 				-- Select next/previous item with Tab / Shift + Tab
 				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
+					if luasnip.locally_jumpable(1) then
+						luasnip.jump(1)
 					else
 						fallback()
 					end
 				end, { "i", "s" }),
+
 				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.locally_jumpable(-1) then
+					if luasnip.locally_jumpable(-1) then
 						luasnip.jump(-1)
 					else
 						fallback()

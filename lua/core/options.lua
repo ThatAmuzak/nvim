84cc04987f7b7da79869dbc38578396fa69a7fd5
ThatAmuzak <rishavbanerjee1710@gmail.com>
2025-06-11 09:29:20 -0700

    spellcheck toggler

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -49,3 +49,7 @@ vim.api.nvim_create_user_command("CdToGitRoot", function()
 	vim.cmd("cd " .. fallback)
 	print("No git root found. Changed directory to file location: " .. fallback)
 end, {})
+function ToggleSpell()
+	vim.wo.spell = not vim.wo.spell
+end
+vim.api.nvim_create_user_command("ToggleSpell", ToggleSpell, {})

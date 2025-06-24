8f091c471e30e2d1d25f8d8ad909bd0cd0ad1e3f
ThatAmuzak <rishavbanerjee1710@gmail.com>
2025-06-24 03:26:33 -0700

    neorg keymap clashes fixed

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -16,6 +16,14 @@ return {
 					},
 				},
 			},
+			vim.keymap.set("n", "<leader>ntt", "<Plug>(neorg.qol.todo-items.todo.task-cycle)"),
+			vim.keymap.set("n", "<leader>nta", "<Plug>(neorg.qol.todo-items.todo.task-ambiguous)"),
+			vim.keymap.set("n", "<leader>ntd", "<Plug>(neorg.qol.todo-items.todo.task-done)"),
+			vim.keymap.set("n", "<leader>nth", "<Plug>(neorg.qol.todo-items.todo.task-on-hold)"),
+			vim.keymap.set("n", "<leader>nti", "<Plug>(neorg.qol.todo-items.todo.task-important)"),
+			vim.keymap.set("n", "<leader>ntp", "<Plug>(neorg.qol.todo-items.todo.task-pending)"),
+			vim.keymap.set("n", "<leader>ntr", "<Plug>(neorg.qol.todo-items.todo.task-recurring)"),
+			vim.keymap.set("n", "<leader>ntu", "<Plug>(neorg.qol.todo-items.todo.task-undone)"),
 		})
 	end,
 }
